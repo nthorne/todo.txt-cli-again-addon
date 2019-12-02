91f44ff9cc26a8dbfb083eefe034880156fec838
munkee <dwhitmarsh@gmail.com>
2019-12-02 14:15:24 -0600

    Threshold filter (#27)
    * Implement 'b' (weekday) interval and unitTest
    
    * Move the today function into againHelpers
    
    * Bash filter to hide events until threshold date
    
    * no op when called as usage to prevent error message

--- againFilter.sh
+++ againFilter.sh
@@ -14,6 +14,7 @@
 
 # You should have received a copy of the GNU General Public License 
 # along with this program. If not, see http://www.gnu.org/licenses/.
+[ "$1" = "usage" ] && exit 0
 
 source `dirname $0`/againHelpers.sh
 
